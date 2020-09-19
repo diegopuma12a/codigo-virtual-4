7cd482b29c86cfac0fb1eb33a99f0b2e515e4b1d
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-09-18 19:48:41 -0500

    Subiendo libro de algoritmos

--- semana3/dia4/01-variables.js
+++ semana3/dia4/01-variables.js
@@ -1,1 +1,22 @@
-console.log("Mi nombre");
+/**
+ * VARIABLES
+ */
+/**
+ * let => es un prefijo que se coloca sólo la primera
+ * vez que se crea una variable
+ * Una variable puede ser modificada o re-utilizada
+ */
+let nombre = "Jorge";
+let apellido = "Garnica";
+let edad = 29;
+console.log(`Edad: ${edad} años`);
+let numero = 10;
+let fechaNacimiento = "09-08-1991";
+
+edad = 28;
+
+console.log(nombre);
+console.log("nombre");
+// concatenar: es unir los valores de dos variables
+console.log("edad " + edad + " años");
+console.log(`nombre ${nombre} y apellido ${apellido}`);
---
+++ semana3/dia4/02-tiposDeDatos.js
@@ -0,0 +1,14 @@
+/**
+ * TIPOS DE DATOS
+ */
+let costoFinal = "no hay :(";
+let titulo = "Ensayo sobre la ceguera";
+let nroPaginas = 185;
+let nuevo = true;
+
+let precio = 55;
+let descuento = 0.1;
+
+costoFinal = precio - precio * descuento;
+
+console.log(`Total a pagar ${costoFinal}`);
