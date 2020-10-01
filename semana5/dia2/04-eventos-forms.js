f3f483092a40a144e16e1daa992b64fe8d8645c8
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-09-30 20:46:52 -0500

    Terminado el ejercicio del formulario y los eventos

--- semana5/dia2/04-eventos-forms.js
+++ semana5/dia2/04-eventos-forms.js
@@ -3,7 +3,41 @@ const link = document.getElementById("link");
 const inputNombre = document.getElementById("inputNombre");
 const inputApellido = document.getElementById("inputApellido");
 const tbody = document.getElementById("tbody");
+const helper = document.getElementById("helper");
+
 let usuarios = [];
+
+const limpiarErrores = () => {
+  // quitar el estilo del brode roojo al formulario
+
+  formulario.classList.remove("borde-rojo");
+
+  // ocultar el small (helper)
+  helper.setAttribute("hidden", true);
+
+  // OJO, si el helper ya estaba oculto, que lo oculte nuevamente
+  // no va generar ningún error en el sistema
+};
+
+const generarError = () => {
+  // va a hacer mostrar el texto de error
+  // elemento.removeAttribute("atributo"), eliminar un atributo de un elemento
+  helper.removeAttribute("hidden");
+  // le va a colocar el borde rojo al formulario
+  formulario.classList.add("borde-rojo");
+};
+
+const eliminarUsuario = (posicion) => {
+  console.log("Eliminando " + posicion);
+
+  usuarios = usuarios.filter((usu, i) => {
+    if (i !== posicion) {
+      return usu;
+    }
+  });
+
+  redibujarTbody();
+};
 
 const redibujarTbody = () => {
   /**
@@ -18,15 +52,46 @@ const redibujarTbody = () => {
    *  6. Reto: darle estilos a las tablas
    */
   tbody.innerHTML = "";
-  let contenido = "";
+
+  // <> </>
+  let tbodyFalso = new DocumentFragment();
+
   usuarios.forEach((usu, i) => {
-    contenido = contenido + `<tr>
-                                <td>${i+1}</td>
-                                <td>${usu.nombre}</td>
-                                <td>${usu.apellido}</td>
-                              </tr>`;
+    let tr = document.createElement("tr");
+
+    let tdNro = document.createElement("td");
+    tdNro.innerText = i + 1;
+
+    let tdNombre = document.createElement("td");
+    tdNombre.innerText = usu.nombre;
+
+    let tdApellido = document.createElement("td");
+    tdApellido.innerText = usu.apellido;
+
+    let tdAcciones = document.createElement("td");
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerText = "Eliminar";
+    btnEliminar.onclick = () => {
+      console.log("se hizo click en eliminar");
+      console.log(`Elimando el registro de: ${usu.nombre}`);
+      eliminarUsuario(i);
+    };
+
+    tdAcciones.appendChild(btnEliminar);
+
+    tr.appendChild(tdNro);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdApellido);
+    tr.appendChild(tdAcciones);
+
+    tbodyFalso.appendChild(tr);
   });
-  tbody.innerHTML = contenido;
+
+  tbody.appendChild(tbodyFalso);
+
+  // colocar el cursor nuevamente en el campo del nombre para que el usaurio
+  // esté listo para ingresar un siguiente registro
+  inputNombre.focus();
 };
 
 /**
@@ -44,12 +109,30 @@ formulario.addEventListener("submit", (evento) => {
   evento.preventDefault();
   console.log("Procesando el formulario...");
 
+  // validar si los campos están llenos
+  if (inputNombre.value.trim() === "" || inputApellido.value.trim() === "") {
+    // indicar un error de ingreso de datos
+
+    generarError();
+
+    return;
+  }
+
   let objUsuario = {
     nombre: inputNombre.value,
     apellido: inputApellido.value,
   };
 
   usuarios.push(objUsuario);
+  // limpiar los campos del formulario
+  inputNombre.value = "";
+  inputApellido.value = "";
+
+  // limpiar los errores
+  // OJO, si no tenían errores previamente, no importa, igual no afecta
+  // los estilos de la aplicación
+  limpiarErrores();
+
   console.log(usuarios);
   //RETO: llamar a una función que redibuje el tbody cada vez
   // que nuestro arreglo de usuarios es alterado (cambia de elementos)
