37b60fd781b3997310d785d8d1dad8211c6c0673
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-10-07 20:39:59 -0500

    avance datatables

--- semana6/dia1/bootstrap/10-carousel.js
+++ semana6/dia1/bootstrap/10-carousel.js
@@ -2,3 +2,29 @@ const btnAnterior = document.getElementById("btnAnterior");
 const btnPause = document.getElementById("btnPause");
 const btnSiguiente = document.getElementById("btnSiguiente");
 const btnIr3 = document.getElementById("btnIr3");
+const btnPlay = document.getElementById("btnPlay");
+btnPlay.onclick = () => {
+  $("#carouselId").carousel("cycle");
+};
+btnAnterior.onclick = () => {
+  $("#carouselId").carousel("prev");
+};
+btnPause.onclick = () => {
+  $("#carouselId").carousel("pause");
+};
+btnSiguiente.onclick = () => {
+  $("#carouselId").carousel("next");
+};
+btnIr3.onclick = () => {
+  $("#carouselId").carousel(2);
+};
+
+
+
+// slider.onmouseover = ()=>{
+//   $("slider").carouse("pause");
+// }
+
+// slider.onmouseout = ()=>{
+//   $("slider").carouse("cycle");
+// }
