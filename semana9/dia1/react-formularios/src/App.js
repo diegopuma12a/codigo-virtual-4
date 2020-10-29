13a7911bca1aae2095f3cea9b5961bdd7ba2eb8c
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-10-29 10:17:24 -0500

    colocando header

--- semana9/dia1/react-formularios/src/App.js
+++ semana9/dia1/react-formularios/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 import Mascotas from './mascotas/Mascotas'
 
 const App = () => {
   return (
     <>
-
+      <Header />
       <main className="container mt-4">
-
         <Mascotas />
-
       </main>
-
     </>
   )
 }
