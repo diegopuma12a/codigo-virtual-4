ccd8b492889c09d41e684c1534846fab5e41ae3a
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-11-02 20:32:08 -0500

    Iniciando proyecto para probar CONTEXT API REACT!

---
+++ semana10/react-context/src/context/contador/contadorContext.js
@@ -0,0 +1,7 @@
+// Crear un contexto
+
+import { createContext } from "react";
+
+const ContadorContext = createContext();
+
+export default ContadorContext;
---
+++ semana10/react-context/src/context/contador/contadorState.js
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react'
+
+const ContadorState = () => {
+
+  const [state, setState] = useReducer(() => { }, 0);
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ContadorState
